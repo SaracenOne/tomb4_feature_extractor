7f6fc2c6c7b4270c1ff1cb12f613029c7e60b114
Saracen <SaracenOne@gmail.com>
2023-09-08 05:14:17 +0100

    Add support for font table width and height extraction

--- leikkuri.py
+++ leikkuri.py
@@ -2,6 +2,10 @@ import struct
 import json
 import numpy as np
 
+
+HEIGHT_ADDRESS = 0x000A728C
+WIDTH_ADDRESS = 0x000A7290
+
 VERTICAL_SPACING_ADDRESS = 0x000A7278
 COMPRESSED_TEXT_FACTOR_ADDRESS = 0x000A7294
 FONT_TABLE_ADDRESS = 0x000ACD98
@@ -38,6 +42,12 @@ def extract_font_data_from_exe(f):
 			bottom_shade = font_table_entry["bottom_shade"]
 			wf.write("{" + f"{u}F,{v}F,{w},{h},{y_offset},{top_shade},{bottom_shade}" + "},\n")
 
+	f.seek(WIDTH_ADDRESS)
+	width = round(struct.unpack('<f', f.read(4))[0], ROUNDING_POINT)
+
+	f.seek(HEIGHT_ADDRESS)
+	height = round(struct.unpack('<f', f.read(4))[0], ROUNDING_POINT)
+
 	f.seek(VERTICAL_SPACING_ADDRESS)
 	vertical_spacing = round(struct.unpack('<f', f.read(4))[0], ROUNDING_POINT)
 
@@ -45,8 +55,10 @@ def extract_font_data_from_exe(f):
 	compressed_text_factor = round(struct.unpack('<f', f.read(4))[0], ROUNDING_POINT)
 
 	with open("font_info.txt", "w") as wf:
-		wf.write(f"vertical_spacing: {vertical_spacing}F")
-		wf.write(f"compressed_text_factor: {compressed_text_factor}F")
+		wf.write(f"width: {width}F\n")
+		wf.write(f"height: {height}F\n")
+		wf.write(f"vertical_spacing: {vertical_spacing}F\n")
+		wf.write(f"compressed_text_factor: {compressed_text_factor}F\n")
 
 
 def read_exe_file(exe_file_path):
